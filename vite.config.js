62f7e393bee81f2ee38945fc95ac28f4ec2b195f
Faig Ismayilov <faigyi@code.edu.az>
2024-11-22 10:14:03 +0400

    some changess

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  base: '/visamazbaku/',
 })

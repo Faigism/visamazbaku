235a879e6a3402e8dacba25e9e110e921c0a6047
Faig Ismayilov <faigyi@code.edu.az>
2024-11-23 11:07:12 +0400

    edit project

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDVlRzfLEHgZDHF8brQyVqoQvIvgIkrcnA',
-  authDomain: 'visamaz-69bdf.firebaseapp.com',
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: 'visamaz-69bdf',
-  storageBucket: 'visamaz-69bdf.firebasestorage.app',
-  messagingSenderId: '521376424647',
-  appId: '1:521376424647:web:d74bf4909c2c2e096b14e2',
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
